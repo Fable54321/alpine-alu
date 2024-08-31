f7878f0a74f49b41f675897d1328b5f9eb8ab9b4
Timothe Bissonnette <timothe5@hotmail.com>
2024-08-31 15:50:18 -0400

    fixed the desktop version of products page

--- src/Products/Products.js
+++ src/Products/Products.js
@@ -17,119 +17,122 @@ import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy } fro
 const Products = () => {
 
     const options = {
-        delay: 1000,
-        duration: 1200,
-        smooth: true,
-    }
+      delay: 1000,
+      duration: 1200,
+      smooth: true,
+    };
 
     useEffect(() => {
-        scroll.scrollTo(110, options)
-    },[])
-
+      scroll.scrollTo(110, options);
+    }, []);
 
     const [index, setIndex] = useState(0);
 
-    const picture =[
-    {
+    const picture = [
+      {
         src: picture1,
         title: "Le standard",
-        descripiton: "Notre modèle Standard est proposé à un prix compétitif et convient à la plupart des installations de cheminée.",
-    },
-    {
+        descripiton:
+          "Notre modèle Standard est proposé à un prix compétitif et convient à la plupart des installations de cheminée.",
+      },
+      {
         src: picture2,
         title: "Le classique",
-        descripiton: "Le modèle Classique est un choix parfait pour ceux qui cherchent à ajouter du style et de l'élégance à leur cheminée, sans compromettre leur budget.",
-    },
-    {
+        descripiton:
+          "Le modèle Classique est un choix parfait pour ceux qui cherchent à ajouter du style et de l'élégance à leur cheminée, sans compromettre leur budget.",
+      },
+      {
         src: picture3,
         title: "L'élégant",
-        descripiton: "Si vous cherchez à ajouter une touche de style et d'élégance à votre cheminée, le chapeau Élégant est un excellent choix.",
-    },
-    {
+        descripiton:
+          "Si vous cherchez à ajouter une touche de style et d'élégance à votre cheminée, le chapeau Élégant est un excellent choix.",
+      },
+      {
         src: picture4,
         title: "Le moderne",
-        descripiton: "Cette cheminée a été spécialement conçue pour s'adapter aux styles de construction contemporains du 21ème siècle.",
-    },
-    {
+        descripiton:
+          "Cette cheminée a été spécialement conçue pour s'adapter aux styles de construction contemporains du 21ème siècle.",
+      },
+      {
         src: picture5,
         title: "L'euro",
-        descripiton: "Le modèle Euro est un hommage à l'Europe traditionnelle, ajoutant une touche de charme à tous les styles de maison et de revêtements.",
-    },
-    {
+        descripiton:
+          "Le modèle Euro est un hommage à l'Europe traditionnelle, ajoutant une touche de charme à tous les styles de maison et de revêtements.",
+      },
+      {
         src: picture6,
         title: "Le rustique",
-        descripiton: "Le style rustique revient en force ! Et pour retrouver l'apparence authentique d'un toit en tôle pincée d'antan, rien de mieux que le modèle Rustique.",
-    },
-    {
+        descripiton:
+          "Le style rustique revient en force ! Et pour retrouver l'apparence authentique d'un toit en tôle pincée d'antan, rien de mieux que le modèle Rustique.",
+      },
+      {
         src: picture7,
         title: "Le contemporain",
-        descripiton: "Le design contemporain de ce modèle s'adapte parfaitement aux maisons d'architecture unique et moderne.",
-    },
-    {
+        descripiton:
+          "Le design contemporain de ce modèle s'adapte parfaitement aux maisons d'architecture unique et moderne.",
+      },
+      {
         src: picture8,
         title: "Le Mont-royal",
-        descripiton: "Un modèle spécialement conçu pour les cheminées de maçonnerie. Cependant, comme tous nos modèles, celui-ci peut être installé sur tout type de revêtement. ",
-    },
-
-    
-];
+        descripiton:
+          "Un modèle spécialement conçu pour les cheminées de maçonnerie. Cependant, comme tous nos modèles, celui-ci peut être installé sur tout type de revêtement. ",
+      },
+    ];
 
     const isPortrait = useMediaQuery({
-        query: '(orientation: portrait)'
-      })
+      query: "(orientation: portrait)",
+    });
 
     const switchUp = () => {
-       if(index === 0) {
-        setIndex(picture.length - 1)
-       }else {
+      if (index === 0) {
+        setIndex(picture.length - 1);
+      } else {
         setIndex(index - 1);
-       }
-
-
-    }  
+      }
+    };
 
     const switchDown = () => {
-        if(index === picture.length - 1) {
-         setIndex(0)
-        }else {
-         setIndex(index + 1);
-        }
- 
- 
-     }  
+      if (index === picture.length - 1) {
+        setIndex(0);
+      } else {
+        setIndex(index + 1);
+      }
+    };
 
-  return (
-    <div className={Styles["products-main"]}>
-      {isPortrait ? (
-        <div className={Styles.productsTextBox}>
-          <h1>Nos produits</h1>
-        </div>
-      ) : null}
-      <div className={Styles.productsPicturesBox}>
-        <img
-          onClick={switchUp}
-          className={Styles["arrow-up"]}
-          src={arrow}
-          alt="arrow Up"
-        />
-        <img
-          className={Styles.cap}
-          src={picture[index].src}
-          alt={picture.title}
-        />
-        <img
-          onClick={switchDown}
-          className={Styles["arrow-down"]}
-          src={arrow}
-          alt="arrow down"
-        />
-        <div className={Styles.bottom}>
-          <h3>{picture[index].title}</h3>
-          <h4>{picture[index].descripiton}</h4>
+    return (
+      <div className={Styles["products-main"]}>
+        
+          <div className={Styles.productsTextBox}>
+            <h1>Nos produits</h1>
+          </div>
+        
+        <div className={Styles.desktop}>
+          <div className={Styles.productsPicturesBox}>
+            <img
+              onClick={switchUp}
+              className={Styles["arrow-up"]}
+              src={arrow}
+              alt="arrow Up"
+            />
+            <img
+              className={Styles.cap}
+              src={picture[index].src}
+              alt={picture.title}
+            />
+            <img
+              onClick={switchDown}
+              className={Styles["arrow-down"]}
+              src={arrow}
+              alt="arrow down"
+            />
+          </div>
+          <div className={Styles.bottom}>
+            <h3>{picture[index].title}</h3>
+            <h4>{picture[index].descripiton}</h4>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
 }
 
 export default Products
